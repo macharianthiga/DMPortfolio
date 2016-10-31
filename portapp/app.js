5a9e6593ba81ea9bea17460f145e965534bbbc1b
dennisfromkenya <dmdmasha28@gmail.com>
2016-10-31 16:44:37 +0000

    Data persistence-Save all projects to db(manually)and make sure that they display properly

--- portapp/app.js
+++ portapp/app.js
@@ -16,11 +16,20 @@ app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 
 //creating my projects
-// Project.create({
-//     image: "./images/03.png",
-//     name: "Zilla Paper",
-//     description: "A creative freelance writing platform"
-// }, function(err, project){
+// Project.create(
+//     {image: "./images/01.png", name: "WeCustomwrite",description: "A creative freelance writing platform"},
+//     {image: "./images/02.png", name: "Phantom Project",description: "Men's watch e-commerce store"},
+//     {image: "./images/03.png", name: "Hey Shabby Me", description: "Ceramic mugs and Clothing ecommerce store"},
+//     {image: "./images/04.png", name: "Zilla Paper",description: "Creative writing platform"},
+//     {image: "./images/05.png", name: "Agrinnov Kenya",description: "Agricultural exports website"},
+//     {image: "./images/06.png", name: "Shazzi Gallery",description: "A simple personal photography gallery"},
+//     {image: "./images/07.png", name: "Embark.org",description: "Adventure sharing platform"},
+//     {image: "./images/08.png", name: "Heroic Imagination",description: "A simple event website for HIP"},
+//     {image: "./images/09.png", name: "Travella",description: "An ongoing exclusive travel app"},
+//     {image: "./images/10.png", name: "DevClad",description: "Ongoing-Confidential"},
+//     {image: "./images/11.png", name: "Confidential",description: "Details after launch"},
+//     {image: "./images/12.png", name: "Confidential",description: "Details will be available after launch"},
+//     function(err, project){
 //     if(err){
 //         console.log(err)
 //     }else{
@@ -32,7 +41,16 @@ app.use(methodOverride("_method"));
 //Routes
         //index root
 app.get("/", function(req, res){
-   res.render("index") ;
+    //get all projects from the database
+    Project.find({}, function(err, allProjects){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", {projects: allProjects});
+            
+        }
+    });
+    
 });
 
  //start the server
