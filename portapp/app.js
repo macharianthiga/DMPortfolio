a5812bd54071fda31076bef0487613895715aac6
dennisfromkenya <dmdmasha28@gmail.com>
2016-11-02 17:33:10 +0000

    Finish styling the index and about me pages

--- portapp/app.js
+++ portapp/app.js
@@ -17,18 +17,18 @@ app.use(methodOverride("_method"));
 
 //creating my projects
 // Project.create(
-//     {image: "./images/01.png", name: "WeCustomwrite",description: "A creative freelance writing platform"},
-//     {image: "./images/02.png", name: "Phantom Project",description: "Men's watch e-commerce store"},
-//     {image: "./images/03.png", name: "Hey Shabby Me", description: "Ceramic mugs and Clothing ecommerce store"},
-//     {image: "./images/04.png", name: "Zilla Paper",description: "Creative writing platform"},
-//     {image: "./images/05.png", name: "Agrinnov Kenya",description: "Agricultural exports website"},
-//     {image: "./images/06.png", name: "Shazzi Gallery",description: "A simple personal photography gallery"},
-//     {image: "./images/07.png", name: "Embark.org",description: "Adventure sharing platform"},
-//     {image: "./images/08.png", name: "Heroic Imagination",description: "A simple event website for HIP"},
-//     {image: "./images/09.png", name: "Travella",description: "An ongoing exclusive travel app"},
-//     {image: "./images/10.png", name: "DevClad",description: "Ongoing-Confidential"},
-//     {image: "./images/11.png", name: "Confidential",description: "Details after launch"},
-//     {image: "./images/12.png", name: "Confidential",description: "Details will be available after launch"},
+//     {image: "./images/01.png", name: "WeCustomwrite", stack:"PHP", url:"http://wecustomwrite.com/", description: "A creative freelance writing platform"},
+//     {image: "./images/02.png", name: "Phantom Project",stack:"Ruby on Rails", url:"http://www.phantomproject.co.uk/", description: "Men's watch e-commerce store"},
+//     {image: "./images/03.png", name: "Hey Shabby Me", stack:"Rails", url:"https://heyshabbyme.com/", description: "Ceramic mugs and Clothing ecommerce store"},
+//     {image: "./images/04.png", name: "Zilla Paper", stack:"PHP(Laravel)", url:"http://zillapaper.com/", description: "Creative writing platform"},
+//     {image: "./images/05.png", name: "Agrinnov Kenya", stack:"Plain HTML & JS", url:"http://agrinnovkenya.co.ke/", description: "Agricultural exports website"},
+//     {image: "./images/06.png", name: "Shazzi Gallery",  stack:"Node Js", url:"Pending Deployment", description: "A simple personal photography gallery"},
+//     {image: "./images/07.png", name: "Embark.org", stack:"PHP", url:"http://www.embark.org/",  description: "Adventure sharing platform"},
+//     {image: "./images/08.png", name: "Heroic Imagination", stack:"Node Js", url:"http://heroicimagination.org/london-event", description: "A simple event website for HIP"},
+//     {image: "./images/09.png", name: "Travella", stack:"iOS 10 & Swift 3", url:"Pending Deployment", description: "An ongoing exclusive travel app"},
+//     {image: "./images/10.png", name: "DevClad", stack:"iOS 10 & Swift 3", url:"Pending Deployment", description: "Ongoing-Confidential"},
+//     {image: "./images/11.png", name: "Confidential", stack:"Go Lang", url:"Pending Deployment", description: "Details after launch"},
+//     {image: "./images/12.png", name: "Confidential", stack:"Node Js", url:"Pending Deployment", description: "Details will be available after launch"},
 //     function(err, project){
 //     if(err){
 //         console.log(err)
@@ -51,6 +51,24 @@ app.get("/", function(req, res){
         }
     });
     
+});
+
+//about me route
+app.get("/about", function(req, res){
+    res.render("about")
+});
+//show route
+app.get("/:id", function(req, res){
+    Project.findById(req.params.id, function(err,foundProject){
+        if(err){
+            res.redirect("/");
+        } else{
+            res.render("show", {project: foundProject});
+            // console.log(foundProject);
+        }
+    });
+    
+   
 });
 
  //start the server
--- portapp/models/index.js
+++ portapp/models/index.js
@@ -5,7 +5,9 @@ var mongoose =require("mongoose");
 var projectSchema = new mongoose.Schema({
     image: String,
     name: String,
-    description: String
+    description: String,
+    stack: String,
+    url: String
 });
 
 module.exports = mongoose.model("Project", projectSchema);
